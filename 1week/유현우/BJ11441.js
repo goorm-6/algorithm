1ca62b96fc5abbb99610ff9bfbf87b74e22af59b
uhanuu <hwryu990927@gmail.com>
2024-01-04 17:00:22 +0900

    백준11441 - 누적합

--- 1week/유현우/BJ11441.js
+++ 1week/유현우/BJ11441.js
@@ -3,16 +3,16 @@ console.log(solution(input));
 
 function solution(input) {
     const answer = [];
-    const [N, M] = input.shift().split(' ').map(num => Number(num));
+    const N = +input.shift();
     const matrix = input.shift().split(' ').map(num => Number(num));
-    
-    let prefixSum = Array.from( { length: N + 1} ).fill(0);
+    const prefixSum = Array.from( {length: N + 1}).fill(0);
 
     for (let i = 0; i < N; i++) {
-        prefixSum[i + 1] = prefixSum[i] +    matrix[i];
+        prefixSum[i + 1] = prefixSum[i] + matrix[i];
     }
 
-    for (let range of input) {
+    const M = +input.shift();
+    for (const range of input) {
         const [start, end] = range.split(' ').map(num => Number(num));
         answer.push(prefixSum[end] - prefixSum[start - 1]);
     }
