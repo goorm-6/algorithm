5ab771c3682914cde03b4d098f50b6e8a2d8fa63
uhanuu <hwryu990927@gmail.com>
2024-01-15 14:32:01 +0900

    백준 2309 - 완전탐색

---
+++ 3week/유현우/BJ2309.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
+
+let result = null;
+console.log(solution(input));
+
+
+function solution(input) {
+    const sum = input.reduce((a, b) => a + b, 0);
+
+    for (let i = 0; i < input.length - 1; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (sum - input[i] - input[j] === 100) {
+                result = input.filter(x => x !== input[i] && x !== input[j]);
+                break;
+            }
+        }
+        if(result) {
+            break;
+        }
+    }
+
+    return result.sort((a, b) => a - b).join('\n');
+}
