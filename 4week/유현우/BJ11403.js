3624e8fe5ab1199a2cace4e538ece0fd672df310
uhanuu <hwryu990927@gmail.com>
2024-01-24 23:17:43 +0900

    백준 11403번

--- 4week/유현우/BJ11403.js
+++ 4week/유현우/BJ11403.js
@@ -1,6 +1,7 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
-const input = fs.readFileSync(filePath).toString().split("\n");
+const input = `3
+0 1 0
+0 0 1
+1 0 0`.split("\n");
 
 const N = +input.shift();
 
@@ -15,6 +16,7 @@ function solution(N, matrix) {
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < N; j++) {
         if (matrix[i][k] && matrix[k][j]) {
+          console.log(`i=${i} j=${j} k=${k}`);
           matrix[i][j] = 1;
         }
       }
